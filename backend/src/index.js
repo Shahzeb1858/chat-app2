1919648938d1f776cb7e9864ae01945c5a8f8369
Shahzeb1858 <shahzebkhan1858@gmail.com>
2025-03-12 19:44:26 +0530

    third commit

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-// const express = require('express') - when we are using a "type" : "common"
-
-const app = express()
-
-app.listen(5001, () => {
-    console.log('Server is ready to run on port 5001');
-})
