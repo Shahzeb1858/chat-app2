3c08dd4e30036ef2befd5adf835d19150835eb8c
Shahzeb1858 <shahzebkhan1858@gmail.com>
2025-03-12 23:00:07 +0530

    sixth commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
+import { dbConnection } from './lib/db.js'
 
 const app = express()
 
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT,()=>{
     console.log('Server is running on port number PORT : ', PORT);  
+    dbConnection()
 })
